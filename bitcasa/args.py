26d4493600283bd1a071a740d145a9ddb52ccd45
Michael Chase <mikec@justyo.co>
2015-07-29 11:10:19 -0700

    Add list and download args
    Centralize defaults
    
    cli args override config file

--- bitcasa/args.py
+++ bitcasa/args.py
@@ -12,6 +12,7 @@ class BitcasaParser(argparse.ArgumentParser):
             parser_class=argparse.ArgumentParser)
 
         self.create_base_parser()
+        self.create_iobase_parser()
         self.create_authentication_parser()
         self.create_action_parsers()
         self.create_shell_parser()
@@ -22,11 +23,31 @@ class BitcasaParser(argparse.ArgumentParser):
 
     def create_action_parsers(self):
         self.list_parser = self.actions.add_parser('list',
-            parents=[self.base_parser], help=('List the root of your '
-                                              'bitcasa drive'))
+            parents=[self.base_parser, self.iobase_parser],
+            help='List the root of your bitcasa drive')
 
-        self.list_parser.add_argument('-d', '--max-depth', dest='max_depth',
-            type=int, default=1, help='Set the depth of the list traversal')
+        self.download_parser = self.actions.add_parser('download',
+            parents=[self.base_parser, self.iobase_parser],
+            help='Recursively download your bitcasa drive')
+
+        self.download_parser.add_argument('--download-workers',
+            dest='download_workers', type=int,
+            help=('The number of workers that can download at one time. '
+                  '(default: 4)'))
+
+        self.download_parser.add_argument('--move-workers',
+            dest='move_workers', type=int,
+            help=('The number of workers that can move at one time. '
+                  '(default: 4)'))
+
+        self.download_parser.add_argument('--download-folder',
+            dest='download_folder',
+            help=('The base folder for downloaded files. '
+                  '(default: ./downloads)'))
+
+        self.download_parser.add_argument('--move-to',
+            dest='move_to',
+            help='The base folder to move completed downloads')
 
     def create_shell_parser(self):
         self.shell_parser = self.actions.add_parser('shell',
@@ -34,15 +55,32 @@ class BitcasaParser(argparse.ArgumentParser):
             help='Launch an interactive authenticated shell')
 
         self.shell_parser.add_argument('--noauth', dest='auth',
-            action='store_false', default=True,
-            help='Do not attempt authentication')
+            action='store_false', help='Do not attempt authentication')
 
         self.shell_parser.add_argument('--pdb', dest='pdb',
             action='store_true', help='Go into a pdb trace on error')
 
+    def create_iobase_parser(self):
+        self.iobase_parser = argparse.ArgumentParser(add_help=False)
+
+        self.iobase_parser.add_argument('-f', '--bitcasa-folder',
+            dest='bitcasa_folder',
+            help='Set the base64 folder path to start list/download')
+
+        self.iobase_parser.add_argument('--sqlite-uri', dest='sqlite_uri',
+            help=('sqlite connection string for SQLAlchemy to connect. '
+                  '(default: sqlite:///bitcasajobs.sqlite'))
+
+        self.iobase_parser.add_argument('--list-workers',
+            dest='list_workers', type=int,
+            help=('How many workers will traverse folders at the same time. '
+                  '(default: 4)'))
+
+        self.iobase_parser.add_argument('-d', '--max-depth', dest='max_depth',
+            type=int, help='The maximum folder traversal depth. (default: 1)')
+
     def create_base_parser(self):
         self.base_parser = argparse.ArgumentParser(add_help=False)
-        self.base_parser.set_defaults(auth=True, pdb=False)
 
         self.base_parser.add_argument('-u', '--username', dest='username',
             help='The username/email for authentication')
@@ -50,22 +88,26 @@ class BitcasaParser(argparse.ArgumentParser):
         self.base_parser.add_argument('-p', '--password', dest='password',
             help='The password for authentication')
 
-        self.base_parser.add_argument('--cookies', dest='cookie_file',
-            type=argparse.FileType('a+'), default='cookies', nargs='?',
-            help='File to store/load cookies')
-
         self.base_parser.add_argument('--chunk-size', type=int,
-            default=1024*1024, dest='chunk_size',
-            help='Size in bytes to download at a time')
+            dest='chunk_size',
+            help='Size in bytes to download at a time. (default: 1048576)')
 
         self.base_parser.add_argument('-n', '--connections', type=int,
-            dest='max_connections', help=('The maximum number of connections'
-                                          ' to make to bitcasa'))
+            dest='max_connections',
+            help='The maximum number of connections to make to bitcasa')
+
+        self.base_parser.add_argument('--cookie-file', dest='cookie_file',
+            nargs='?', help='Path to the cookie file. (default: ./cookies)')
 
         self.base_parser.add_argument('-c', '--config', dest='config_file',
-            type=argparse.FileType('a+'), default='./bitcasa.ini', nargs='?',
-            help='Path to the config file. ')
+            nargs='?', help='Path to the config file. (default: ./bitcasa.ini)')
+
+        self.base_parser.add_argument('--log-file', dest='log_file',
+            nargs='?', help='Path to the log file. (default: no logging)')
+
+        self.base_parser.add_argument('-q', '--quiet', dest='quiet',
+            action='store_true',
+            help='Quiet mode: Disables console logging.')
 
         self.base_parser.add_argument('-v', '--verbose', dest='verbose',
-            action='count')
-
+            action='count', help='Increase verbosity')
--- bitcasa/config.py
+++ bitcasa/config.py
@@ -1,4 +1,5 @@
 import json
+import re
 import traceback
 
 from ConfigParser import ConfigParser, Error
@@ -10,40 +11,57 @@ from .models import SimpleObject
 class Config(SimpleObject):
     _keys = []
 
+    def __contains__(self, key):
+        return hasattr(self, key)
+
 class ConfigManager(object):
 
     def __init__(self, cli_args, config_file=None):
         cli_args = cli_args.__dict__.copy()
 
-        self.config = Config(**cli_args)
+        self.config = Config(**self.get_defaults())
+        print self.config.__dict__
         self.cli_args = cli_args
 
         cli_config_file = cli_args.pop('config_file', None)
-        self.config_file = config_file or cli_config_file
+        self.config_file = (config_file or cli_config_file or
+                            self.config.config_file)
         if self.config_file:
             data = self.read_config()
             self.config.update_data(**data)
+            print self.config.__dict__
+
+        updated = dict([(key, val) for key, val in cli_args.items()
+                        if key not in self.config or val is not None])
+        self.config.update_data(**updated)
+        print self.config.__dict__
+
+    @classmethod
+    def get_defaults(cls):
+        defaults = dict(auth=True, pdb=False, move_workers=4, list_workers=4,
+                        download_workers=4,max_depth=1, cookie_file='./cookies',
+                        chunk_size=1024*1024, config_file='./bitcasa.ini',
+                        quiet=False, download_folder='./downloads',
+                        sqlite_uri='sqlite:///bitcasajobs.sqlite')
+        return defaults
 
     def _read_sections(self, config):
         data = {}
         for section in config.sections():
             for key, val in config.items(section):
-                if key in ['cookie_file']:
-                    val = open(val, 'a+')
-                if key in ['pdb', 'auth']:
+                dash_key = key.replace('-', '_')
+                if val.lower() in ['true', 'false']:
                     val = config.getboolean(section, key)
-                if key in ['chunk_size', 'max_connections', 'max_depth',
-                           'verbose']:
+                elif re.match(r'^[0-9]+$', val):
                     val = config.getint(section, key)
 
-                data[key] = val
+                data[dash_key] = val
 
         return data
 
     def read_config(self):
         config = ConfigParser()
-        self.config_file.seek(0)
-        config.read(self.config_file.name)
+        config.read(self.config_file)
 
         try:
             data = self._read_sections(config)
